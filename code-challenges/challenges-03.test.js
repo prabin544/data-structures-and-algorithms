4d1f3ccf968a188ef3e62782967b3aa430064817
prabin singh <pravin544@gmail.com>
2021-03-29 22:43:48 -0500

    added first challenege

--- code-challenges/challenges-03.test.js
+++ code-challenges/challenges-03.test.js
@@ -7,7 +7,11 @@ Write a function called addTwo that takes in an array and adds two to every valu
 ------------------------------------------------------------------------------------------------ */
 
 const addTwo = (arr) => {
-  // Solution code here...
+  let newarr = [];
+  arr.map((i)=>{
+    newarr.push(i+2)
+  })
+  return newarr
 };
 
 /* ------------------------------------------------------------------------------------------------
