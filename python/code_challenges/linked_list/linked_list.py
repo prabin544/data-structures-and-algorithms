1f5cbf9839663e3206d699cfa1e2d4265f427a51
Prabin Singh <pravin544@gmail.com>
2021-06-20 09:47:35 -0500

    added code 3 challenege

---
+++ python/code_challenges/linked_list/linked_list.py
@@ -0,0 +1,49 @@
+class Node:
+
+    def __init__(self, value, next=None):
+        self.value = value
+        self.next = next
+
+class LinkedList:
+
+    def __init__(self, node=None):
+        self.head = node
+
+    def insert(self, value):
+
+        node = Node(value)
+        node.next = self.head
+        self.head = node
+        return self
+
+    def includes(self, value):
+
+        current = self.head
+
+        while current is not None:
+            if current.value == value:
+                return True
+            current = current.next
+        return False
+
+    def __str__(self):
+
+        string = ""
+
+        current = self.head
+
+        while current is not None:
+            string += f"{ {current.value} } ->"
+            current = current.next
+
+        string += f" None "
+
+        return string
+
+if __name__ == "__main__":
+
+    ll1 = LinkedList()
+    ll1.insert("a").insert("b").insert("c").insert("d")
+
+    result = str(ll1)
+    print(result)
--- python/tests/test_linked_list.py
+++ python/tests/test_linked_list.py
@@ -1,5 +1,64 @@
-from linked_list.linked_list import LinkedList
+from code_challenges.linked_list.linked_list import LinkedList, Node
 
+def test_a():
+    node1 = Node("apple")
+    actual = node1.value
+    expected = "apple"
+    assert actual == expected
+    assert node1.next == None
 
-def test_import():
-    assert LinkedList
+# Can successfully instantiate an empty linked list
+def test_one():
+    linked_list1 = LinkedList()
+    actual = linked_list1.head
+    expected = None
+    assert actual == expected
+
+# Can properly insert into the linked list
+def test_two():
+    ll2 = LinkedList()
+    ll2.insert("a")
+    ll2.insert("b")
+    actual = ll2.head.value
+    expected = "b"
+    assert actual == expected
+
+# The head property will properly point to the first node in the linked list
+def test_three():
+    node1 = Node("a")
+    ll1 = LinkedList(node1)
+    actual = ll1.head.value
+    expected = "a"
+    assert actual == expected
+
+# Can properly insert multiple nodes into the linked list
+def test_four():
+    ll1 = LinkedList()
+    ll1.insert("a").insert("b").insert("c")
+    actual = ll1.head.value
+    expected = "c"
+    assert actual == expected
+
+# Will return true when finding a value within the linked list that exists
+def test_five():
+    ll1 = LinkedList()
+    ll1.insert("a").insert("b").insert("c").insert("d")
+    actual = ll1.includes("c")
+    expected = True
+    assert actual == expected
+
+# Will return false when searching for a value in the linked list that does not exist
+def test_six():
+    ll1 = LinkedList()
+    ll1.insert("a").insert("b").insert("c").insert("d")
+    actual = ll1.includes("h")
+    expected = False
+    assert actual == expected
+
+# Can properly return a collection of all the values that exist in the linked list
+def test_seven():
+    ll1 = LinkedList()
+    ll1.insert("a").insert("b").insert("c").insert("d")
+    actual = str(ll1)
+    expected = "{'d'} ->{'c'} ->{'b'} ->{'a'} -> None "
+    assert actual == expected
