974144f81edad97b8cdc70446e8fcd7662c7481d
Prabin Singh <pravin544@gmail.com>
2021-07-28 21:23:45 -0500

    added solutions

--- python/tests/test_stack_queue_brackets.py
+++ python/tests/test_stack_queue_brackets.py
@@ -3,52 +3,52 @@ from stack_queue_brackets.stack_queue_brackets import validate_brackets
 
 def test_case_one():
     actual = validate_brackets("{}")
-    expected = (True, "Balanced")
+    expected = (True, "Balanced Brackets")
     assert actual == expected
 
 def test_case_two():
     actual = validate_brackets("{}(){}")
-    expected = (True, "Balanced")
+    expected = (True, "Balanced Brackets")
     assert actual == expected
 
 def test_case_three():
     actual = validate_brackets("()[[Extra Characters]]")
-    expected = (True, "Balanced")
+    expected = (True, "Balanced Brackets")
     assert actual == expected
 
 def test_case_four():
     actual = validate_brackets("(){}[[]]")
-    expected = (True, "Balanced")
+    expected = (True, "Balanced Brackets")
     assert actual == expected
 
 def test_case_five():
     actual = validate_brackets("{}{Code}[Fellows](())")
-    expected = (True, "Balanced")
+    expected = (True, "Balanced Brackets")
     assert actual == expected
 
 def test_case_six():
     actual = validate_brackets("[({}]")
-    expected = (False, "Unbalanced")
+    expected = (False, "Unbalanced Brackets")
     assert actual == expected
 
 def test_case_seven():
     actual = validate_brackets("(](")
-    expected = (False, "Unbalanced")
+    expected = (False, "Unbalanced Brackets")
     assert actual == expected
 
 def test_case_eight():
     actual = validate_brackets("{(})")
-    expected = (False, "Unbalanced")
+    expected = (False, "Unbalanced Brackets")
     assert actual == expected
 
 def test_case_nine():
     actual = validate_brackets("")
-    expected = (True, "Balanced")
+    expected = (True, "Balanced Brackets")
     assert actual == expected
 
 def test_case_ten():
-    actual = validate_brackets("daniel")
-    expected = (True, "Balanced")
+    actual = validate_brackets("Prabin")
+    expected = (True, "Balanced Brackets")
     assert actual == expected
 
 # def test_new_node():
